6b06a9a0cf01e3c7425230e777aa5b60ded78766
Nguyễn Lê Ngọc Dương <duongnln@ghn.vn>
2023-01-09 10:25:00 +0700

    chap02 shopping cart cookies

--- chap_02/redis_test.go
+++ chap_02/redis_test.go
@@ -19,7 +19,7 @@ func TestLoginCookies(t *testing.T) {
 	client := clientRepo.NewClientRepo(connector)
 	token := uuid.New().String()
 
-	t.Run("Test Login cookies", func(t *testing.T) {
+	t.Run("login_cookies", func(t *testing.T) {
 		client.UpdateToken(token, "duongnln", "itemX")
 		t.Log("Logged-in/update token: ", token)
 		t.Log("For user: ", "duongnln")
@@ -43,6 +43,30 @@ func TestLoginCookies(t *testing.T) {
 		s := connector.Conn.HLen("login:").Val()
 		t.Log("The current number of sessions still available is:", s)
 		utils.AssertnumResult(t, 1, s)
+		defer client.Reset()
+	})
+
+	t.Run("shopping_cart_cookies", func(t *testing.T) {
+		t.Log("Refresh session...")
+		client.UpdateToken(token, "duongnln", "itemX")
+		t.Log("And add an item to the shopping cart")
+		client.AddToCart(token, "itemY", 3)
+		client.AddToCart(token, "itemZ", 2)
+		r := connector.Conn.HGetAll("cart:" + token).Val()
+		t.Log("Our shopping cart currently has:", r)
+
+		utils.AssertTrue(t, len(r) >= 1)
+
+		t.Log("Let's clean out our sessions and carts")
+		common.LIMIT = 1
+		go client.CleanUpFullSession()
+		time.Sleep(1 * time.Second)
+		common.QUIT = true
+		time.Sleep(2 * time.Second)
+		utils.AssertThread(t, common.FLAG)
+
+		r = connector.Conn.HGetAll("cart:" + token).Val()
+		t.Log("Our shopping cart now contains:", r)
 		defer client.Reset()
 	})
 }
--- chap_02/repo/client/base.go
+++ chap_02/repo/client/base.go
@@ -35,6 +35,14 @@ func (r *clientRepo) CheckToken(token string) string {
 	return r.Connector.Conn.HGet("login:", token).Val()
 }
 
+// UpdateToken
+//
+// login - hash - key token value user
+//
+// zset contains score and member (score is used to sort)
+// recent - set sort by timestamp
+//
+// view - set sort by timestamp
 func (r *clientRepo) UpdateToken(token, user, item string) {
 	timestamp := time.Now().Unix()
 	r.Connector.Conn.HSet("login:", token, user)
@@ -74,6 +82,43 @@ func (r *clientRepo) CleanUpSession() {
 		r.Connector.Conn.Del(sessionKeys...)
 		r.Connector.Conn.HDel("login:", tokens...)
 		r.Connector.Conn.ZRem("recent:", tokens)
+	}
+
+	defer atomic.AddInt32(&common.FLAG, -1)
+}
+
+// cart is hash table with name cart:token contains key is item and value is count
+func (r *clientRepo) AddToCart(session, item string, count int) {
+	if count <= 0 {
+		r.Connector.Conn.HDel(fmt.Sprintf("cart:%s", session), item)
+	} else {
+		r.Connector.Conn.HSet(fmt.Sprintf("cart:%s", session), item, count)
+	}
+}
+
+// Over time, memory use will grow, and we’ll want to clean out old data
+func (r *clientRepo) CleanUpFullSession() {
+	for !common.QUIT {
+		size := r.Connector.Conn.ZCard("recent:").Val()
+		if size <= common.LIMIT {
+			time.Sleep(1 * time.Second)
+			continue
+		}
+
+		endIndex := utils.Min(size-common.LIMIT, 100)
+		// log.Printf("===endIndex===: %d", endIndex)
+		tokens := r.Connector.Conn.ZRange("recent:", 0, endIndex-1).Val()
+		// log.Printf("===tokens===: %+v", tokens)
+
+		var sessionKeys []string = make([]string, 0, len(tokens))
+		for _, token := range tokens {
+			sessionKeys = append(sessionKeys, fmt.Sprintf("viewed:%s", token))
+			sessionKeys = append(sessionKeys, fmt.Sprintf("cart:%s", token))
+		}
+
+		r.Connector.Conn.Del(sessionKeys...)
+		r.Connector.Conn.HDel("login:", sessionKeys...)
+		r.Connector.Conn.ZRem("recent:", sessionKeys)
 	}
 
 	defer atomic.AddInt32(&common.FLAG, -1)
